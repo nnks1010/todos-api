e2d9ff9966f7bf27c2afb26106957b97fdb63801
nnks1010 <ntomoya1010@gmail.com>
2018-12-08 02:14:27 +0900

    update user model and add test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # encrypt passowrd
+  has_secure_password
+
+  # Model associations
+  has_many :todos, foreign_key: :created_by
+
+  # Validations
+  validates_presence_of :name, :email, :password_digest
 end
